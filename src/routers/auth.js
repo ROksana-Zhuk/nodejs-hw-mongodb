335da42933ee0239b1553fd96e06fd7ab187933d
ROksana-Zhuk <roksi.sun.forever@gmail.com>
2025-08-01 22:23:20 +0300

    hw-7 finished

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-import { loginUserSchema, registerUserSchema, requestResetEmailSchema, resetPasswordSchema } from '../validation/auth.js';
-import { loginUserController, logoutUserController, refreshUserSessionController, registerUserController, requestResetEmailController, resetPasswordController } from '../controllers/auth.js';
+import { loginUserSchema, loginWithGoogleOAuthSchema, registerUserSchema, requestResetEmailSchema, resetPasswordSchema } from '../validation/auth.js';
+import { getGoogleOAuthUrlController, loginUserController, loginWithGoogleController, logoutUserController, refreshUserSessionController, registerUserController, requestResetEmailController, resetPasswordController } from '../controllers/auth.js';
 import { validateBody } from '../middlewares/validateBody.js';
 
 const router = Router();
@@ -34,5 +34,22 @@ router.post(
     validateBody(resetPasswordSchema),
     ctrlWrapper(resetPasswordController),
 );
+
+
+router.get('/get-oauth-url', ctrlWrapper(getGoogleOAuthUrlController));
+
+
+router.post(
+    '/confirm-oauth',
+    validateBody(loginWithGoogleOAuthSchema),
+    ctrlWrapper(loginWithGoogleController),
+);
+
+
+
+
+
+
+
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -14,6 +14,7 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 
 const PORT = Number(getEnvVar('PORT', '3000'));
@@ -42,6 +43,8 @@ export const setupServer = () => {
   });
 
   app.use(router);
+
+  app.use('/api-docs', swaggerDocs());
 
   app.use(notFoundHandler);
 
